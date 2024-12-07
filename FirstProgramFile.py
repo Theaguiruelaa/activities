35ad2701c41c37abaa89a00533ad6fa97b305b68
Theaguiruelaa <theaguiruela@gmail.com>
2024-12-07 20:27:17 +0800

    Add files via upload

---
+++ FirstProgramFile.py
@@ -0,0 +1,1 @@
+print ("naknang tokwa")
---
+++ code_challenge2.py
@@ -0,0 +1,3 @@
+name = input ("What is your name?---> ")
+
+print ("\t\t\t\t\t\t\t\t\t\t\t * \n\t\t\t\t\t\t\t\t\t\t\t\t\t\b\b\b       * * * \n\t\t\t\t\t\t\t\t\t\t\t\t\b\b      * * * * \n\t\t\t\t\t\t\t\t\t\t\t\b    * Hi, " + name + " * \n\t\t\t\t\t\t\t\t\t\t\t\b      * * * * \n\t\t\t\t\t\t\t\t\t\t\t\b       * * * \n\t\t\t\t\t\t\t\t\t\t\t *")
