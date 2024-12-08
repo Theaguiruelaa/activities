0be8b6bbfd637e9ad713c18652c5a39029fd3116
Theaguiruelaa <theaguiruela@gmail.com>
2024-12-08 09:01:54 +0800

    Add files via upload

---
+++ Activity4.py
@@ -0,0 +1,7 @@
+number1 = eval(input("enter a number -->  "))
+number2 = eval(input("enter a number --> "))
+
+answer1= number1 + number2 
+
+
+print("The sum of " , number1 , " and " , number2 , " is " , answer1)
