0cbcdb55726a56bf35e6b23a4b313483ffa0736a
Theaguiruelaa <theaguiruela@gmail.com>
2024-12-13 18:22:02 +0800

    Add files via upload

---
+++ Activity8.py
@@ -0,0 +1,12 @@
+password = input ("Enter your password: ")
+
+if password.lower() == "secret":
+		print("Access Granted")
+		print("Enjoy using the system")
+elif password.lower() == "alammobading":
+		print("yes bading")
+		print("Access Granted")
+else: 
+		print("Access Denied")
+
+print("System Exit")
