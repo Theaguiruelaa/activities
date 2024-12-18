ff299b79a572511a8e43145339ee4df19df283bf
Theaguiruelaa <theaguiruela@gmail.com>
2024-12-18 21:08:43 +0800

    Delete code_challenge2.py

--- code_challenge2.py
+++
@@ -1,3 +0,0 @@
-name = input ("What is your name?---> ")
-
-print ("\t\t\t\t\t\t\t\t\t\t\t * \n\t\t\t\t\t\t\t\t\t\t\t\t\t\b\b\b       * * * \n\t\t\t\t\t\t\t\t\t\t\t\t\b\b      * * * * \n\t\t\t\t\t\t\t\t\t\t\t\b    * Hi, " + name + " * \n\t\t\t\t\t\t\t\t\t\t\t\b      * * * * \n\t\t\t\t\t\t\t\t\t\t\t\b       * * * \n\t\t\t\t\t\t\t\t\t\t\t *")
